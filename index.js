baa236ba97663029598bb09d6208afe52547ab94
Ahmed Fasih <wuzzyview@gmail.com>
2018-03-13 04:40:46 -0400

    Switch to Fortran-default

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const flatten = require('callbag-flatten');
 const map = require('callbag-map');
 
-function cartesian(cOrder, ...args) {
-  // cOrder is either a boolean or a callbag, in either case, if its falsey, then we want Fortran order
-  if (!cOrder) {
-    return args.reduceRight(
-        (bag, b) => flatten(map(outer => map(inner => [inner].concat(outer))(b))(bag)), map(x => [x])(args.pop()));
+function cartesian(fOrder, ...args) {
+  if (!fOrder) {
+    return args.reduce(
+        (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(args.shift()));
   }
-  // Next, is it a boolean? (In which case it must be true)
-  if (typeof cOrder === 'boolean') {
-    return args.slice(1).reduce(
-        (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(args[0]));
-  }
-  // Or is it a callbag, in which case we assume a default of C ordering
-  return args.reduce(
-      (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(cOrder));
+  if (fOrder !== true) { args.unshift(fOrder); }
+  return args.reduceRight(
+      (bag, b) => flatten(map(outer => map(inner => [inner].concat(outer))(b))(bag)), map(x => [x])(args.pop()));
 }
 
 module.exports = { cartesian };
--- tests.js
+++ tests.js
@@ -2,9 +2,9 @@ const test = require('tape');
 const { pipe, fromIter, concat, filter, map, forEach } = require('callbag-basics');
 
 const { cartesian, cartesian2, cartesian3, cartesian4, cartesian5 } = require('./index');
-const array1 = Array.from(Array(50), (_, i) => '' + i);
+const array1 = Array.from(Array(49), (_, i) => '' + i);
 const array2 = 'abcdefghi';
-const array3 = Array.from(Array(50), _ => '' + Math.random());
+const array3 = Array.from(Array(51), _ => '' + Math.random());
 // C ordering: first element varies the slowest
 let expectedC = [];
 for (let a1 of array1) {
@@ -20,32 +20,37 @@ for (let a3 of array3) {
   }
 }
 
+function elapsed(start) {
+  const end = process.hrtime(start);
+  return Math.round(end[0] * 1000 + end[1] / 1e6);
+}
+
 test('no arguments', t => {
   let actual = [];
+  let tic = process.hrtime();
   pipe(cartesian(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
-  t.equal(expectedC.length, actual.length, "same length");
-  t.deepEqual(expectedC, actual, "same values");
+  tic = elapsed(tic);
+  t.comment('ms f/defa ' + tic);
+  t.deepEqual(expectedF, actual, "same values");
+  t.end();
+});
+
+test('F order', t => {
+  let actual = [];
+  let tic = process.hrtime();
+  pipe(cartesian(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
+  tic = elapsed(tic);
+  t.comment('ms f/bool: ' + tic);
+  t.deepEqual(expectedF, actual, "same values");
   t.end();
 });
 
 test('C order', t => {
   let actual = [];
-  pipe(cartesian(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
-  t.equal(expectedC.length, actual.length, "same length");
+  let tic = process.hrtime();
+  pipe(cartesian(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
+  tic = elapsed(tic);
+  t.comment('ms c/bool ' + tic);
   t.deepEqual(expectedC, actual, "same values");
   t.end();
 });
-
-test('Fortran ordering', t => {
-  let actual = [];
-  pipe(cartesian(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
-
-  t.equal(expectedF.length, actual.length, "same length");
-  t.deepEqual(expectedF, actual, "same values");
-
-  t.end();
-});
-function elapsed(start) {
-  const end = process.hrtime(start);
-  return Math.round(end[0] * 1000 + end[1] / 1e6);
-}
