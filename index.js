90a46c974e9a91387b103473d32037b38a1decac
Ahmed Fasih <wuzzyview@gmail.com>
2018-03-13 03:47:23 -0400

    Timing tests on a bunch of different implementations

--- index.js
+++ index.js
@@ -18,9 +18,81 @@ function cartesian(cOrder, ...args) {
   return bag;
 }
 
+function cartesian2(cOrder, ...args) {
+  let bag, startAt = 0;
+  if (typeof cOrder === 'boolean') {
+    if (!cOrder) { args.reverse(); }
+    bag = map(x => [x])(args[0]);
+    startAt = 1;
+  } else {
+    bag = map(x => [x])(cOrder);
+    startAt = 0;
+    cOrder = true;
+  }
+  for (let i = startAt; i < args.length; i++) {
+    bag = flatten(map(outer => map(inner => cOrder ? outer.concat(inner) : [ inner ].concat(outer))(args[i]))(bag));
+  }
+  return bag;
+}
+function cartesian3(cOrder, ...args) {
+  let bag, startAt = 0;
+  if (typeof cOrder === 'boolean') {
+    if (!cOrder) { args.reverse(); }
+    bag = map(x => [x])(args[0]);
+    startAt = 1;
+  } else {
+    bag = map(x => [x])(cOrder);
+    startAt = 0;
+    cOrder = true;
+  }
+  if (cOrder) {
+    for (let i = startAt; i < args.length; i++) {
+      bag = flatten(map(outer => map(inner => outer.concat(inner))(args[i]))(bag));
+    }
+  } else {
+    for (let i = startAt; i < args.length; i++) {
+      bag = flatten(map(outer => map(inner => [inner].concat(outer))(args[i]))(bag));
+    }
+  }
+  return bag;
+}
+function cartesian4(cOrder, ...args) {
+  if (!cOrder) {
+    return args.reduceRight(
+        (bag, b) => flatten(map(outer => map(inner => [inner].concat(outer))(b))(bag)), map(x => [x])(args.pop()));
+  }
+  // cOrder is either True or a callbag
+  if (typeof cOrder === 'boolean') {
+    return args.slice(1).reduce(
+        (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(args[0]));
+  }
+  return args.reduce(
+      (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(cOrder));
+}
+function cartesian5(cOrder, ...args) {
+  if (!cOrder) {
+    return args.reduceRight(
+        (bag, b) => flatten(map(outer => map(inner => [inner].concat(outer))(b))(bag)), map(x => [x])(args.pop()));
+  }
+  // cOrder is either True or a callbag
+  if (typeof cOrder === 'boolean') {
+    let bag = map(x => [x])(args[0]);
+    for (let i = 1; i < args.length; i++) {
+      bag = flatten(map(outer => map(inner => outer.concat(inner))(args[i]))(bag));
+    }
+    return bag;
+  }
+  return args.reduce(
+      (bag, b) => flatten(map(outer => map(inner => outer.concat(inner))(b))(bag)), map(x => [x])(cOrder));
+}
+
 // function cartesianIter(...args) { return cartesian(...args.map(fromIter)); }
 
 module.exports = {
   cartesian,
+  cartesian2,
+  cartesian3,
+  cartesian4,
+  cartesian5
   // cartesianIter,
 };
--- tests.js
+++ tests.js
@@ -1,10 +1,10 @@
 const test = require('tape');
 const { pipe, fromIter, concat, filter, map, forEach } = require('callbag-basics');
 
-const { cartesian } = require('./index');
-const array1 = Array.from(Array(3), (_, i) => i);
-const array2 = 'abc' || 'abcdefghi';
-const array3 = Array.from(Array(3), _ => Math.random());
+const { cartesian, cartesian2, cartesian3, cartesian4, cartesian5 } = require('./index');
+const array1 = Array.from(Array(50), (_, i) => '' + i);
+const array2 = 'abcdefghi';
+const array3 = Array.from(Array(50), _ => '' + Math.random());
 // C ordering: first element varies the slowest
 let expectedC = [];
 for (let a1 of array1) {
@@ -45,3 +45,129 @@ test('Fortran ordering', t => {
 
   t.end();
 });
+function elapsed(start) {
+  const end = process.hrtime(start);
+  return Math.round(end[0] * 1000 + end[1] / 1e6);
+}
+test('others', t => {
+  let actual2 = [], actual3 = [], actual4 = [], actual5 = [], actual = [];
+  let tic;
+
+  tic = process.hrtime();
+  pipe(cartesian2(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual2.push(x)));
+  const tf2 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian3(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual3.push(x)));
+  const tf3 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian4(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual4.push(x)));
+  const tf4 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian5(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual5.push(x)));
+  const tf5 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian(false, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
+  const tf = elapsed(tic);
+
+  t.deepEqual(expectedF, actual2, "same values");
+  t.deepEqual(expectedF, actual3, "same values");
+  t.deepEqual(expectedF, actual4, "same values");
+  t.deepEqual(expectedF, actual5, "same values");
+
+  actual2 = [];
+  actual3 = [];
+  actual4 = [];
+  actual5 = [];
+  actual = [];
+  tic = process.hrtime();
+  pipe(cartesian2(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual2.push(x)));
+  const tc2 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian3(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual3.push(x)));
+  const tc3 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian4(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual4.push(x)));
+  const tc4 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian5(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual5.push(x)));
+  const tc5 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian(fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
+  const tc = elapsed(tic);
+
+  t.deepEqual(expectedC, actual2, "same values");
+  t.deepEqual(expectedC, actual3, "same values");
+  t.deepEqual(expectedC, actual4, "same values");
+  t.deepEqual(expectedC, actual5, "same values");
+
+  actual2 = [];
+  actual3 = [];
+  actual4 = [];
+  actual5 = [];
+  actual = [];
+  tic = process.hrtime();
+  pipe(cartesian2(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual2.push(x)));
+  const sc2 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian3(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual3.push(x)));
+  const sc3 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian4(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual4.push(x)));
+  const sc4 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian5(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual5.push(x)));
+  const sc5 = elapsed(tic);
+  tic = process.hrtime();
+  pipe(cartesian(true, fromIter(array1), fromIter(array2), fromIter(array3)), forEach(x => actual.push(x)));
+  const sc = elapsed(tic);
+
+  t.deepEqual(expectedC, actual2, "same values");
+  t.deepEqual(expectedC, actual3, "same values");
+  t.deepEqual(expectedC, actual4, "same values");
+  t.deepEqual(expectedC, actual5, "same values");
+
+  console.log('f/bool', tf2, tf3, tf4, tf5, tf);
+  console.log('c/bool', tc2, tc3, tc4, tc5, tc);
+  console.log('c/DEFA', sc2, sc3, sc4, sc5, sc);
+
+  t.end();
+});
+
+/*
+Timing results:
+        c2  c3  c4  c5  orig
+c/DEFA  10  10  11  11  19
+c/DEFA  10  10  11  15  10
+c/DEFA  10  10  11  17  10
+c/DEFA  10  10  11  17  10
+c/DEFA  10  10  11  18  10
+c/DEFA  10  10  11  18  10
+c/DEFA  11  10  11  18  10
+c/DEFA  11  10  11  18  10
+c/DEFA  11  10  18  12  10
+c/DEFA  12  10  11  18  10
+
+c/bool  11  10  10  10  19
+c/bool  11  10  11  11  18
+c/bool  11  10  11  11  19
+c/bool  11  10  11  11  19
+c/bool  11  10  11  19  10
+c/bool  11  11  11  11  19
+c/bool  11  11  11  11  20
+c/bool  12  11  11  11  19
+c/bool  12  13  18  12  11
+c/bool  16  10  11  11  19
+
+f/bool  11  11  7   7   20
+f/bool  12  11  7   8   21
+f/bool  12  12  7   7   20
+f/bool  12  12  7   8   22
+f/bool  12  12  8   8   21
+f/bool  12  13  7   8   21
+f/bool  13  12  7   7   21
+f/bool  13  13  8   8   24
+f/bool  15  8   9   15  13
+f/bool  16  7   7   13  13
+
+Conclusions: c4, with the three reductions, is totally competitive.
+*/
